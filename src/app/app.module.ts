82ad501223b78f1219b2f7a5f031875931d5e865
yefferson-caleno <yefferson.caleno@o365.unab.edu.co>
2021-11-01 18:58:34 -0500

    maquetado de pantalla para administración de equipos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { TeamsComponent } from './pages/teams/teams.component';
+import { TeamModalComponent } from './components/modals/team-modal/team-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    TeamsComponent,
+    TeamModalComponent
   ],
   imports: [
     BrowserModule,
