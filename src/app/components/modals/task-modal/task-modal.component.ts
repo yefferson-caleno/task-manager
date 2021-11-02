2bc347bd80f3598ef41bd999820308f5825053c1
yefferson-caleno <yefferson.caleno@o365.unab.edu.co>
2021-11-01 21:27:36 -0500

    maquetado de pantalla para administración de tareas

---
+++ src/app/components/modals/task-modal/task-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-modal',
+  templateUrl: './task-modal.component.html',
+  styleUrls: ['./task-modal.component.scss']
+})
+export class TaskModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
