7543750e6afb09f6e2fabff40e5022415cf05643
yefferson-caleno <yefferson.caleno@o365.unab.edu.co>
2021-11-01 21:59:20 -0500

    maquetado de pantalla para inicio de sesión

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
 import { HomeComponent } from './pages/home/home.component';
 import { TasksComponent } from './pages/tasks/tasks.component';
 import { TeamsComponent } from './pages/teams/teams.component';
 import { UsersComponent } from './pages/users/users.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent
+  },
   {
     path: 'home',
     component: HomeComponent
